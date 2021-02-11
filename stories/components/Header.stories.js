0769614e4413bed09abbca27b98a31262bd2f362
CrisRonda <cristian.j005@gmail.com>
2021-02-10 20:12:33 -0500

    remove comments

--- stories/components/Header.stories.js
+++ stories/components/Header.stories.js
@@ -1,14 +1,9 @@
 import Banner from "../../src/screens/Home";
 const Story = () => <Banner />;
 
-// Here we export a variant of the default template passing props
 export const HeaderStory = Story.bind({});
 HeaderStory.args = {};
 
-// Here we export a variant of the default template passing props
-
-// Here we export the default component that
-// will be used by Storybook to show it inside the sidebar
 export default {
   title: "Components/Header",
   component: Banner,
