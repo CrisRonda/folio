83a583eea79e45eaf90c200084ce673ba40d3998
CrisRonda <cristian.j005@gmail.com>
2021-02-12 17:08:09 -0500

    fix name

--- stories/components/Header.stories.js
+++ stories/components/Header.stories.js
@@ -1,10 +1,10 @@
-import Banner from "../../src/screens/Home";
-const Story = () => <Banner />;
+import Header from "../../src/screens/Home";
+const Story = () => <Header />;
 
 export const HeaderStory = Story.bind({});
 HeaderStory.args = {};
 
 export default {
   title: "Components/Header",
-  component: Banner,
+  component: Header,
 };
